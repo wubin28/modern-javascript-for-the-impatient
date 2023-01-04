fef3a721f9cc1f1e2c303051f579a52d555aac16
Ben WU <wubinben@icloud.com>
2023-01-04 22:15:47 +0800

    refactor: add describe in the test code

--- ch-01-values-and-variables/ch-01-values-and-variables.test.js
+++ ch-01-values-and-variables/ch-01-values-and-variables.test.js
@@ -1,5 +1,7 @@
 const add = require('./ch-01-values-and-variables');
 
-test('1.2 typeof null is the string object', () => {
-    expect(typeof null).toMatch('object');
+describe("Chapter 1. Values And Variables", () => {
+    test('1.2 typeof null is the string object', () => {
+        expect(typeof null).toMatch('object');
+    });
 });
