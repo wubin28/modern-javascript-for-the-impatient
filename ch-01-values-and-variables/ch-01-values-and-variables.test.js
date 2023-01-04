c300439057b6ac9ad320ddcfa3b065f04e3af8f4
Ben WU <wubinben@icloud.com>
2023-01-04 22:32:23 +0800

    test: 1.4 variables do not have a type

--- ch-01-values-and-variables/ch-01-values-and-variables.test.js
+++ ch-01-values-and-variables/ch-01-values-and-variables.test.js
@@ -1,11 +1,17 @@
 const add = require('./ch-01-values-and-variables');
 
-describe("Chapter 1. Values And Variables", () => {
-    test('1.2 typeof null is the string object', () => {
+describe('Chapter 1. Values And Variables', () => {
+    it('1.2 typeof null is the string object', () => {
         expect(typeof null).toMatch('object');
     });
 
-    test("1.4 if you do not initialize a variable, it has the special value undefined", () => {
+    it('1.4 variables do not have a type', () => {
+        let counter = 0;
+        counter = 'zero';
+        expect(counter).toMatch('zero');
+    });
+
+    it('1.4 if you do not initialize a variable, it has the special value undefined', () => {
         let x;
         expect(x).toBeUndefined();
     });
