f32c58fab6ec80425e742c06bcbdd5f1a666eff7
Ben WU <wubinben@icloud.com>
2023-01-04 22:23:13 +0800

    test: 1.4 if you do not initialize a variable, it has the special value undefined

--- ch-01-values-and-variables/ch-01-values-and-variables.test.js
+++ ch-01-values-and-variables/ch-01-values-and-variables.test.js
@@ -4,4 +4,9 @@ describe("Chapter 1. Values And Variables", () => {
     test('1.2 typeof null is the string object', () => {
         expect(typeof null).toMatch('object');
     });
+
+    test("1.4 if you do not initialize a variable, it has the special value undefined", () => {
+        let x;
+        expect(x).toBeUndefined();
+    });
 });
