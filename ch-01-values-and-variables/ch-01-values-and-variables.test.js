fa0031c8da3fb1bc7482d73762bbb90e2efff428
Ben WU <wubinben@icloud.com>
2023-01-05 08:36:25 +0800

    test: 1.6: some number-producing functions return NaN to indicate a faulty input

--- ch-01-values-and-variables/ch-01-values-and-variables.test.js
+++ ch-01-values-and-variables/ch-01-values-and-variables.test.js
@@ -1,22 +1,22 @@
 const add = require('./ch-01-values-and-variables');
 
-describe('Chapter 1. Values And Variables', () => {
-    it('1.2 typeof null is the string object', () => {
+describe('Chapter 1: Values And Variables', () => {
+    it('1.2: typeof null is the string object', () => {
         expect(typeof null).toMatch('object');
     });
 
-    it('1.4 variables do not have a type', () => {
+    it('1.4: variables do not have a type', () => {
         let counter = 0;
         counter = 'zero';
         expect(counter).toMatch('zero');
     });
 
-    it('1.4 if you do not initialize a variable, it has the special value undefined', () => {
+    it('1.4: if you do not initialize a variable, it has the special value undefined', () => {
         let x;
         expect(x).toBeUndefined();
     });
 
-    it('1.4 if you try to modify the value contained in a const, a run-time error occurs', () => {
+    it('1.4: if you try to modify the value contained in a const, a run-time error occurs', () => {
         const badFunction = () => {
             const PI = 3.14;
             PI = 3.1416;
@@ -25,14 +25,14 @@ describe('Chapter 1. Values And Variables', () => {
         expect(badFunction).toThrow();
     });
 
-    it('1.4 avoid two obsolete forms of variable declarations, with the var keyword and with no keyword at all (misspelling creates a new variable)', () => {
+    it('1.4: avoid two obsolete forms of variable declarations, with the var keyword and with no keyword at all (misspelling creates a new variable)', () => {
         var counter = 0; // Obsolete
         coutner = 1; // Note the misspelling - creates a new variable
         expect(counter).toBe(0);
         expect(coutner).toBe(1);
     });
 
-    it('1.6 JavaScript has no explicit integer type. All numbers are double-precision floating-point numbers.', () => {
+    it('1.6: JavaScript has no explicit integer type. All numbers are double-precision floating-point numbers.', () => {
         const aFloat = parseFloat('3.0');
         const anInt = parseInt('3');
         expect(aFloat).toBe(anInt);
@@ -40,16 +40,20 @@ describe('Chapter 1. Values And Variables', () => {
         expect(typeof anInt).toMatch('number');
     });
 
-    it('1.6 the toString method converts a number back to a string', () => {
+    it('1.6: the toString method converts a number back to a string', () => {
         expect((3.0).toString()).toMatch('3');
     });
 
-    it('1.6 if you divide by zero, the result is Infinity or -Infinity', () => {
+    it('1.6: if you divide by zero, the result is Infinity or -Infinity', () => {
         expect((3 / 0).toString()).toMatch('Infinity');
         expect((-3 / 0).toString()).toMatch('-Infinity');
     });
 
-    it('1.6 0/0 is NaN, the "not a number" constant', () => {
+    it('1.6: 0/0 is NaN, the "not a number" constant', () => {
         expect(0/0).toBeNaN();
+    });
+
+    it('1.6: some number-producing functions return NaN to indicate a faulty input', () => {
+        expect(parseFloat('pie')).toBeNaN();
     });
 });
